e11f2285b5adb7ba66deda1f919420e7589976aa
KOMATSU Satoshi <komatsu-sa@gnavi.co.jp>
2019-01-15 19:08:42 +0900

    [wip]

--- src/components/structure/Candidatenamesarea.vue
+++ src/components/structure/Candidatenamesarea.vue
@@ -2,35 +2,40 @@
   .Candidatenames
     .field.is-grouped
       .control.is-expanded
-        input.input.is-size-4(type="text", placeholder="Input Name", v-model='candidateItem', @keypress.enter='addListname(candidateItem)')
+        input.input.is-size-4(type="text", placeholder="Input Name", v-model.trim='$store.candidatename', @keypress.enter='addListname(candidateItem)')
       .control
-        a.button.is-primary.is-large(@click='addListname(candidateItem)') Add Name
+        a.button.is-primary.is-large(@click='addCandidateName') Add Name
     .Candidatenames_list
       .field.is-grouped.is-grouped-multiline
         //- nameset
         .control(v-for='item in nameItem')
           .tags.are-large.has-addons
-            .tag.is-link.is-large {{ item.name }}
-            a.tag.is-delete.is-large
+            .tag.is-link.is-large {{ isCandidateName }}
+            a.tag.is-delete.is-large(@click='deleteCandidateName')
 </template>
 
 <script>
 export default {
   data: () => {
-    return {
-      nameItem:[
-        { name: 'komatsu-sa' },
-        { name: 'mogehoge' }
-      ],
-      candidateItem: ''
+    return this.$store.candidatename
+  },
+  computed: {
+    isCandidateName() {
+      return this.$store.getters.isCandidatename;
     }
   },
   methods: {
-    addListname: (newName) => {
-      this.nameItem.push({
-        name: newName
-      });
-      this.candidateItem = '';
+    // addListname: (newName) => {
+    //   this.nameItem.push({
+    //     name: newName
+    //   });
+    //   this.candidateItem = '';
+    // },
+    addCandidateName(){
+      this.$store.dispatch('addName');
+    },
+    deleteCandidateName(){
+      this.$store.dispatch('deleteName');
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App,store },
-  template: '<App/>'
+  components: { App },
+  template: '<App/>',
+  store
 })
